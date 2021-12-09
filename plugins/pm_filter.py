ddf172f9e7136fe7a6bc79ba55b7e498a9635945
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-12-09 09:15:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -442,7 +442,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('Connection', callback_data='coct'),
             InlineKeyboardButton('𝖬𝗎𝗍𝖾', callback_data='mute'),
-            InlineKeyboardButton('ADMIN', callback_data='extra'),
+            InlineKeyboardButton('ADMIN', callback_data='extra')
             ],[
             InlineKeyboardButton('𝖨𝗆𝖽𝖻', callback_data='imbd'),
             InlineKeyboardButton('𝖨𝗇𝖿𝗈', callback_data='info'),
